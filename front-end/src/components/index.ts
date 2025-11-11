a3935e4d73d7780d2192980320ea799ab544166b
bearpawmaxim <ocheretianko.m@gmail.com>
2025-11-11 18:43:38 +0200

    Add statsCard.

--- front-end/src/components/index.ts
+++ front-end/src/components/index.ts
@@ -3,5 +3,6 @@ export * from './backButton';
 export * from './dataTable';
 export * from './errorMessage';
 export * from './userAvatar';
+export * from './statCard';
 export * from './themePicker';
 export * from './page';
