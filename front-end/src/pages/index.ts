8592d942a7d6a5732abd56266d7c317ffab8733b
GizmoBoss <gizmoboss@gmail.com>
2025-11-13 22:44:23 +0200

    Remove export for public page from index.ts to streamline module exports.

--- front-end/src/pages/index.ts
+++ front-end/src/pages/index.ts
@@ -5,6 +5,5 @@ export * from './messages/messages';
 export * from './messageEdit/messageEdit';
 export * from './chats/chats'
 export * from './stations/stations';
-export * from './public';
 export * from './users/users';
 export * from './buildings/buildings';
