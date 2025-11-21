670334e4f6c77c8ed9aeb145893f3791bf1120aa
GizmoBoss <gizmoboss@gmail.com>
2025-11-22 00:15:48 +0200

    Update branding and icons in the front-end layout. Replace "Deye monitoring bot" with "Svitlo Power monitoring tool" in multiple locations, add new favicon and apple touch icons, and remove unused SVG files. Enhance public layout styles for better visual consistency.

--- front-end/src/layouts/publicLayout.tsx
+++ front-end/src/layouts/publicLayout.tsx
@@ -1,10 +1,12 @@
 import { FC, ReactNode, useEffect, useState } from "react";
-import { Anchor, AppShell, Box, Container, Group, Title, Transition } from "@mantine/core";
+import { Anchor, AppShell, Box, Container, Group, Image, Title, Transition, useMantineColorScheme } from "@mantine/core";
 import { ThemePicker } from "../components";
 import classes from './styles/publicLayout.module.css';
 import { VisitTracker } from "./components/visitTracker";
 import { Authors } from "./components/authors";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import iconDark from "../assets/icon_dark.png";
+import iconLight from "../assets/icon_light.png";
 
 type PublicLayoutProps = {
   children: ReactNode;
@@ -13,6 +15,8 @@ type PublicLayoutProps = {
 export const PublicLayout: FC<PublicLayoutProps> = ({ children }) => {
   const [isAltPressed, setIsAltPressed] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
+  const { colorScheme } = useMantineColorScheme();
+  const iconSrc = colorScheme === 'dark' ? iconLight : iconDark;
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.altKey) {
@@ -46,7 +50,8 @@ export const PublicLayout: FC<PublicLayoutProps> = ({ children }) => {
         <header className={classes.header}>
           <Container size="md" className={classes.inner}>
             <Group justify="flex-start" align="center">
-              <Title order={2}>Power monitoring</Title>
+              <Image h={40} w={40} src={iconSrc} alt="Logo" />
+              <Title  className={classes.title} order={2}>vitlo Power</Title>
               <Transition transition="slide-down" mounted={isHovering && isAltPressed}>
                 {(transitionStyles) => (
                   <Anchor
